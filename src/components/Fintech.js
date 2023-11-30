c20156aa01ffc9e427228c4e362180daa6ec7b1b
arnonotieno19 <arnonotieno19@gmail.com>
2023-11-30 16:08:45 +0300

    new

--- src/components/Fintech.js
+++ src/components/Fintech.js
@@ -6,7 +6,7 @@ const Fintech = () => {
     <div className={styles.bottomcontainer}>
       <div className={styles.bottomcontent}>
         <div className={styles.bottomitems}>
-          <h1 className={styles.bottomh2}>FINTECH SOLUTIONS<br />DRIVING ECONOMICS <br /><span className={styles.forward}>FORWARD </span><i style={{ fontFamily: 'Anton' }}>FORWARD</i></h1>
+          <h1 className={styles.bottomh2}>INNOVATE, ELEVATE,<br />CAPTIVATE YOUR BUSINESS's <br /><i style={{ fontFamily: 'Anton' }}>ALL-IN-ONE.</i></h1>
         </div>
       </div>
     </div>
